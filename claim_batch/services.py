ac2aa051cf218bc13756729f5617c03d37d3ae64
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-10-04 11:41:53 +0200

    reports bulk version

--- claim_batch/services.py
+++ claim_batch/services.py
@@ -54,5 +54,123 @@ class ProcessBatchService(object):
                     pass
                 finally:
                     next = cur.nextset()
-            if res[0]: # zero means "all done"
+            if res[0]:  # zero means "all done"
                 raise ProcessBatchSubmitError(res[0])
+
+
+def process_batch_report_data_with_claims(prms):
+    with connection.cursor() as cur:
+        sql = """\
+            EXEC [dbo].[uspSSRSProcessBatchWithClaim]
+                @LocationId = %s,
+                @ProdID = %s,
+                @RunID = %s,
+                @HFID = %s,
+                @HFLevel = %s,
+                @DateFrom = %s,
+                @DateTo = %s
+        """
+        cur.execute(sql, (
+            prms.get('locationId', 0),
+            prms.get('prodId', 0),
+            prms.get('runId', 0),
+            prms.get('hfId', 0),
+            prms.get('hfLevel', ''),
+            prms.get('dateFrom', ''),
+            prms.get('dateTo', '')
+        ))
+        # stored proc outputs several results,
+        # we are only interested in the last one
+        next = True
+        data = None
+        while next:
+            try:
+                data = cur.fetchall()
+            except:
+                pass
+            finally:
+                next = cur.nextset()
+    return [{
+        "ClaimCode": r[0],
+        "DateClaimed": r[1].strftime("%Y-%m-%d") if r[1] is not None else None,
+        "OtherNamesAdmin": r[2],
+        "LastNameAdmin": r[3],
+        "DateFrom": r[4].strftime("%Y-%m-%d") if r[4] is not None else None,
+        "DateTo": r[5].strftime("%Y-%m-%d") if r[5] is not None else None,
+        "CHFID": r[6],
+        "OtherNames": r[7],
+        "LastName": r[8],
+        "HFID": r[9],
+        "HFCode": r[10],
+        "HFName": r[11],
+        "AccCode": r[12],
+        "ProdID": r[13],
+        "ProductCode": r[14],
+        "ProductName": r[15],
+        "PriceAsked": r[16],
+        "PriceApproved": r[17],
+        "PriceAdjusted": r[18],
+        "RemuneratedAmount": r[19],
+        "DistrictID": r[20],
+        "DistrictName": r[21],
+        "RegionID": r[22],
+        "RegionName": r[23]
+    } for r in data]
+
+
+def process_batch_report_data(prms):
+    with connection.cursor() as cur:
+        sql = """\
+            EXEC [dbo].[uspSSRSProcessBatch]
+                @LocationId = %s,
+                @ProdID = %s,
+                @RunID = %s,
+                @HFID = %s,
+                @HFLevel = %s,
+                @DateFrom = %s,
+                @DateTo = %s
+        """
+        cur.execute(sql, (
+            prms.get('locationId', 0),
+            prms.get('prodId', 0),
+            prms.get('runId', 0),
+            prms.get('hfId', 0),
+            prms.get('hfLevel', ''),
+            prms.get('dateFrom', ''),
+            prms.get('dateTo', '')
+        ))
+        # stored proc outputs several results,
+        # we are only interested in the last one
+        next = True
+        data = None
+        while next:
+            try:
+                data = cur.fetchall()
+            except:
+                pass
+            finally:
+                next = cur.nextset()
+    return [{
+        "RegionName": r[0],
+        "DistrictName": r[1],
+        "HFCode": r[2],
+        "HFName": r[3],
+        "ProductCode": r[4],
+        "ProductName": r[5],
+        "Remunerated": r[6],
+        "AccCodeRemuneration": r[7],
+        "AccCode": r[8]
+    } for r in data]
+
+
+class ReportDataService(object):
+    def __init__(self, user):
+        self.user = user
+
+    def fetch(self, prms):
+        show_claims = prms.get("showClaims", "false") == "true"
+        if (show_claims):
+            data = process_batch_report_data_with_claims(prms)
+        else:
+            data = process_batch_report_data(prms)
+        return data
