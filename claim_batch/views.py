80eb3511fe9d77f14abe5bcb50566b9ba091105f
Thibault Dethier <tdethier@bluesquarehub.com>
2024-02-16 10:01:13 +0100

    Added invoice generation in excel

--- claim_batch/views.py
+++ claim_batch/views.py
@@ -13,7 +13,7 @@ def _report(prms):
     show_claims = prms.get("showClaims", "false") == 'true'
     group = prms.get("group", "H")
     if show_claims:
-        report = "claim_batch_pbc_"+group
+        report = "claim_batch_pbc_" + group
         default = pbc_H.template if group == 'H' else pbc_P.template
     elif group == 'H':
         report = "claim_batch_pbh"
@@ -29,25 +29,26 @@ def report(request):
     if not user.has_perms(ClaimBatchConfig.account_preview_perms):
         raise PermissionDenied(_("unauthorized"))
     report_service = ReportService(user)
-    report, default = _report(request.GET)
+    report_name, default = _report(request.GET)
     report_data_service = ReportDataService(user)
     try:
         data = report_data_service.fetch(request.GET)
     except ValueError:
         raise Http404(f"No data found")
 
-    result = report_service.process(report,
-                                  {'data': data,
-                                   'DateFrom': request.GET['dateFrom'],
-                                   'DateTo': request.GET['dateTo'],
-                                   'RegionCode': request.GET['regionCode'],
-                                   'RegionName': request.GET['regionName'],
-                                   'HFCode': request.GET['hfCode'],
-                                   'HFName': request.GET['hfName'],
-                                   'HFLevel': request.GET['hfLevel'],
-                                   'ProductCode': request.GET['productCode'],
-                                   'ProductName': request.GET['productName'],
-                                   'RunDate': request.GET['runDate']
-                                   },
-                                  default)
+    result = report_service.process(report_name,
+                                    {'data': data,
+                                     'DateFrom': request.GET['dateFrom'],
+                                     'DateTo': request.GET['dateTo'],
+                                     'RegionCode': request.GET['regionCode'],
+                                     'RegionName': request.GET['regionName'],
+                                     'HFCode': request.GET['hfCode'],
+                                     'HFName': request.GET['hfName'],
+                                     'HFLevel': request.GET['hfLevel'],
+                                     'ProductCode': request.GET['productCode'],
+                                     'ProductName': request.GET['productName'],
+                                     'RunDate': request.GET['runDate']
+                                     },
+                                    default,
+                                    report_format=request.GET['fileFormat'])
     return result
